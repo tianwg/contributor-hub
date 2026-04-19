647a9d6ea954a3b03de3b6adb017bac7012d8ed1
Wei Tian <tianwg@github.com>
2026-04-19 01:41:48 -0400

    Remove committed build artifacts from api/src to fix broken import path

--- midnight-dapp-demo/api/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * as PrivateVault from "./managed/private-vault/contract/index.js";
-export * from "./witnesses";
-//# sourceMappingURL=index.d.ts.map
--- midnight-dapp-demo/api/src/witnesses.js
+++
@@ -1,9 +0,0 @@
-export const createVaultPrivateState = (secretKey) => ({
-    secretKey,
-});
-export const witnesses = {
-    localSecretKey: (context) => {
-        return context.privateState.secretKey;
-    },
-};
-//# sourceMappingURL=witnesses.js.map
